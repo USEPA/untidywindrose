942fe84787f5464374e61bb854bd4a1c06715681
Clinton Mccrowey <mccrowey.clinton@epa.gov>
2021-02-22 08:01:42 -0500

    use rprojroot to set relative paths

--- R/untidywindrose.R
+++ R/untidywindrose.R
@@ -2,9 +2,15 @@
 #' 
 #' @title utwr
 #' @description starts the untidywindrose shiny app
-#' @import shiny
+#' @importFrom shiny runApp
+#' @importFrom magrittr `%>%`
+#' @importFrom rprojroot find_root_file
 #' @return shiny application object
 #' @export
 utwr <- function() {
-  shiny::runApp("./R/app.R")
+  rprojroot::find_root_file("R",
+                            "app.R",
+                            criterion = has_file("DESCRIPTION")
+                            ) %>% 
+  shiny::runApp()
 }
