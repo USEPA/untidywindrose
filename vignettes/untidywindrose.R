7ce929e382a85f8ed3393e196a42883193977e4b
Clinton Mccrowey <mccrowey.clinton@epa.gov>
2021-03-23 14:51:04 -0400

    fix minor spelling mistakes and build errors

--- vignettes/untidywindrose.R
+++ vignettes/untidywindrose.R
@@ -1,35 +1,28 @@
-## ----setup, include = FALSE--------------------------------------------------------------------------------------------
+## ----setup, include = FALSE-------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
 )
 
+## ---- echo=FALSE------------------------------------------------------
+print(getwd())
+#sampledata <- read.table("vignettes/sampledata.txt", header = T, sep = "\t")
+#knitr::kable(sampledata, caption = "Sample windrose data")
 
-## ----child = "EPA_Disclaimer.Rmd", echo = FALSE, comment = NA----------------------------------------------------------
-
-
-
-
-## ---- echo=FALSE-------------------------------------------------------------------------------------------------------
-list.files()
-sampledata <- read.table("./vignettes/sampledata.txt", header = T, sep = "\t")
-knitr::kable(sampledata, caption = "Sample windrose data")
-
-
-## ----echo=FALSE, fig.width=5, fig.align = "center", message=F, warning=F-----------------------------------------------
+## ----echo=FALSE, fig.width=5, fig.align = "center", message=F, warning=F----
 require(openair)
 cbPalette <- c("#999999", "#E69F00", "#56B4E9",  #colorbild safer pallette
                 "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
-openair::windRose(sampledata,
-                  ws = "Windspeed",
-                  auto.text = T,
-                  wd = "WindDirection",
-                  cols = cbPalette,
-                  paddle = T,
-                  statistic = "prop.count",
-                  annotate  = T,
-                  key.position = "right",
-                  #breaks = c(0.5, 3, 10, 15),
-                  #ws.int = 5,
-                  key.footer = "mi/hr")
+# openair::windRose(sampledata,
+#                   ws = "Windspeed",
+#                   auto.text = T,
+#                   wd = "WindDirection",
+#                   cols = cbPalette,
+#                   paddle = T,
+#                   statistic = "prop.count",
+#                   annotate  = T,
+#                   key.position = "right",
+#                   #breaks = c(0.5, 3, 10, 15),
+#                   #ws.int = 5,
+#                   key.footer = "mi/hr")
 
